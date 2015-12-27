c74319780b897042ae571b5f79e052973e7bae41
Jason Lee <jasonlee9@gmail.com>
2015-12-27 08:06:10 -0800

    forgot to add files/dir. argh

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "YamlSwift",
+    dependencies: [
+        // .Package(url: "https://github.com/Zewo/PostgreSQL.git", majorVersion: 0, minor: 1)
+      ]
+)
